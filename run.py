c05c7ef4828986df6865cd427f8b68aa1a4e6177
AlexeyBlinnikov <ablinnikovi@gmail.com>
2023-06-09 21:10:07 +0300

    «second__commit»

--- run.py
+++ run.py
@@ -52,20 +52,6 @@ def price(x, x1, y, y1):
     return float(data["price"])
     # print(x)
 
-# def pobeda():
-#     r = requests.get("https://победа-63.рф/catalog/foto-i-videotehnika/videokamery/?q=20&s=new&c=0&cg=127&a=0&min=15000&max=68900&")
-#     html = BS(r.content, 'html.parser')
-
-#     # print(go)
-
-#     data = html.find_all('div', class_ = 'card is-lazy')
-#     for i in data:
-#         content = i.find("a", class_ = 'card-title')
-#         name = content['title']
-#         if name not in go:
-#             go.append(name)      
-    
-#     return go[-1]
     
 def english_words():
     word_today = random.sample(words, 5)
